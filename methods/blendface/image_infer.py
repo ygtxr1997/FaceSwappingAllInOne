2eb01abc202a2130244cc9cdb3fc78e6b06e6f36
ygtxr1997 <gavingreenson@icloud.com>
2024-02-24 01:16:05 +0000

    update: blendface image_infer

--- methods/blendface/image_infer.py
+++ methods/blendface/image_infer.py
@@ -8,24 +8,57 @@ import torchvision
 from torchvision import transforms
 
 from swapping import BlendSwap
+from align_tools import FaceAlignImageInfer
+
 
 make_abs_path = lambda fn: os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), fn))
 
 
+class BlendFaceImageInfer(object):
+    def __init__(self):
+        device = "cuda:0"
+        weight_path = make_abs_path("checkpoints/blendswap.pth")
+        model = BlendSwap()
+        model.load_state_dict(torch.load(weight_path, map_location='cpu'))
+        model.eval()
+        model.to(device)
+
+        self.device = device
+        self.model = model
+
+    def infer_image(self, source: Image.Image, target: Image.Image):
+        target_img = transforms.ToTensor()(target).unsqueeze(0).to(self.device)
+        source_img = transforms.ToTensor()(source).unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            output = self.model(target_img, source_img)
+        print(output.shape)
+        swapped = Image.fromarray(
+            (output.permute(0, 2, 3, 1)[0].cpu().data.numpy() * 255).astype(np.uint8)
+        )
+        return swapped
+
+
 if __name__ == "__main__":
-    device = "cuda:0"
-    weight_path = make_abs_path("checkpoints/blendswap.pth")
     target_path = make_abs_path("swapping/examples/target.png")
     source_path = make_abs_path("swapping/examples/source.png")
 
-    model = BlendSwap()
-    model.load_state_dict(torch.load(weight_path, map_location='cpu'))
-    model.eval()
-    model.to(device)
+    target_pil = Image.open(target_path)
+    source_pil = Image.open(source_path)
 
-    target_img = transforms.ToTensor()(Image.open(target_path)).unsqueeze(0).to(device)
-    source_img = transforms.ToTensor()(Image.open(source_path)).unsqueeze(0).to(device)
+    aligner = FaceAlignImageInfer()
+    s_aligned, t_aligned, t_affine = aligner.crop_ori_to_aligned(
+        source_pil, target_pil
+    )
+    s_aligned.save("tmp_s_aligned.png")
+    t_aligned.save("tmp_t_aligned.png")
 
-    with torch.no_grad():
-        output = model(target_img, source_img)
-    print(output.shape)
+    blend_face = BlendFaceImageInfer()
+    swapped = blend_face.infer_image(
+        s_aligned, t_aligned
+    )
+
+    pasted = aligner.paste_aligned_to_ori(
+        swapped, target_pil, t_affine
+    )
+    pasted.save("tmp_pasted.png")
